8ebb6a6bbd5247166c66fb68b3a615e1e674edae
Thea <thea.kupler@liefery.com>
2020-05-27 12:49:30 +0200

    first passing test

---
+++ lib/bottles.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+class Bottles
+  def verse(_number)
+    "99 bottles of beer on the wall, 99 bottles of beer.
+Take one down and pass it around, 98 bottles of beer on the wall.
+"
+  end
+end
