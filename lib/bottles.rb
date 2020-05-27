435e7c0a91e10c1c11f8d7bec622c16d36a44509
Thea <thea.kupler@liefery.com>
2020-05-27 17:19:44 +0200

    eigth test

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -30,4 +30,8 @@ class Bottles
     list_of_all_bottles_count = (sorted_begin_smallest[0]..sorted_begin_smallest[1]).to_a
     list_of_all_bottles_count.reverse.map { |bottles_count| Bottles.new.verse(bottles_count) } * "\n"
   end
+
+  def song
+    Bottles.new.verses(99, 0)
+  end
 end
